01509fc85b4d59fa2b8a51260aeb44c5a0886dab
sparshprogrammer <srivastava.sparsh@gmail.com>
2025-01-31 00:59:18 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 nifty_df = pd.read_csv('NIFTY_50.csv')
+
+nifty_df = nifty_df.fillna(method='ffill')
 print(nifty_df)
+
+pythonCopyfrom pycaret.time_series import *
+exp = setup(nifty_df, target='target_column')
+best_model = compare_models()
+print(best_model)
