5d98f9a2b8dfc1cb54ef8f77729fec009ec2f24d
sparshprogrammer <srivastava.sparsh@gmail.com>
2025-02-09 22:14:36 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -6,7 +6,7 @@ nifty_df = nifty_df.fillna(method='ffill')
 print(nifty_df)
 
 from pycaret.time_series import *
-nifty_df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+nifty_df['Date'] = pd.to_datetime(nifty_df['Date'], format='%m/%d/%Y')
 exp = setup(data=nifty_df, target='Close', time_index='Date')
 best_model = compare_models()
 print(best_model)
