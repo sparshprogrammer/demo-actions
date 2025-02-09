25ba432392d746ecf4dbbf06df0aaf1df152b9c7
sparshprogrammer <srivastava.sparsh@gmail.com>
2025-02-09 22:50:32 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -10,5 +10,10 @@ nifty_df['Date'] = pd.to_datetime(nifty_df['Date'], format='%Y-%m-%d')
 # nifty_df.set_index('Date', inplace=True)
 exp = setup(data=nifty_df, target='Close')
 best_model = compare_models()
-# print(best_model)
 plot_model(best_model)
+
+# Forecast the next 10 periods (for example, 10 days)
+forecast = predict_model(best_model, fh=10)
+
+# Show the forecasted results
+print(forecast)
