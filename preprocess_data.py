f01af2a5c27eb59251dba65651b21ff45e6e0f0f
sparshprogrammer <srivastava.sparsh@gmail.com>
2025-02-09 22:34:24 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,7 +5,7 @@ nifty_df = pd.read_csv('NIFTY_50.csv')
 nifty_df = nifty_df.fillna(method='ffill')
 print(nifty_df)
 
-from pycaret.time_series import *
+from pycaret.classification import *
 nifty_df['Date'] = pd.to_datetime(nifty_df['Date'], format='%Y-%m-%d')
 # nifty_df.set_index('Date', inplace=True)
 exp = setup(data=nifty_df, target='Close')
